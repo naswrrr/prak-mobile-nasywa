08e04c753938c367496a280e99f97bc74b1be95b
nasywa <nasywa24si@mahasiswa.pcr.ac.id>
2026-04-10 08:22:42 +0700

    pertemuan 3: penerapan viewBinding

---
+++ app/src/main/java/com/example/egiluyapps/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,41 @@
+package com.example.egiluyapps.pertemuan_3
+
+import android.R.attr.name
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.egiluyapps.R
+import com.example.egiluyapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val nama = binding.inputNoTujuan.text
+            Log.e("Klik btnKirim","Tombol berhasil di tekan. Isi dari inputNama = $nama")
+
+            Toast.makeText(this, "Pesan berhasil terkirim ke: $nama" , Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
