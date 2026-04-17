07376e47a96660d62308bde0ccd3c51ab015127f
nasywa <nasywa24si@mahasiswa.pcr.ac.id>
2026-04-17 07:57:58 +0700

    pertemuan 4 : warming up

--- app/src/main/java/com/example/egiluyapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/egiluyapps/pertemuan_4/FourthActivity.kt
@@ -1,22 +1,27 @@
-package com.example.egiluyapps
+package com.example.egiluyapps.pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.egiluyapps.MainActivity
+import com.example.egiluyapps.R
 import com.example.egiluyapps.databinding.ActivityFourthBinding
-import com.example.egiluyapps.databinding.ActivityMainBinding
-import com.example.egiluyapps.pertemuan_4.FourthActivity
+import com.example.egiluyapps.databinding.ActivityThirdBinding
+import com.example.egiluyapps.pertemuan_3.ThirdResultActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityFourthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding = ActivityFourthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -24,8 +29,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.btnToFourth.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
